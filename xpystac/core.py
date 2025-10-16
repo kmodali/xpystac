ca5e89b7ffe52be288c7d410aa79eae73bb0d70e
k204247 <modali@dkrz.de>
2025-10-16 16:53:31 +0200

    merging from public main branch

--- xpystac/core.py
+++ xpystac/core.py
@@ -1,18 +1,21 @@
 import functools
-from typing import List, Literal, Mapping, Union
+from collections.abc import Callable
 
 import pystac
 import xarray
 import os
 from tqdm import tqdm
 
-from xpystac.utils import _import_optional_dependency, _is_item_search
+from xpystac._xstac_kerchunk import _stac_to_kerchunk
+from xpystac.utils import _import_optional_dependency
 
 
 @functools.singledispatch
 def to_xarray(
     obj,
-    stacking_library: Union[Literal["odc.stac", "stackstac"], None] = None,
+    *,
+    patch_url: None | Callable[[str], str] = None,
+    allow_kerchunk: bool = True,
     **kwargs,
 ) -> xarray.Dataset:
     """Given a PySTAC object return an xarray dataset.
@@ -24,65 +27,57 @@ def to_xarray(
       dataset. If the asset points to a COG, read that.
     * Item: stacks all the assets into a dataset with 1 more dimension than
       any given asset.
-    * ItemCollection (output of pystac_client.search): stacks all the
-      assets in all the items into a dataset with 2 more dimensions than
-      any given asset.
 
     Parameters
     ----------
     obj : PySTAC object (Item, ItemCollection, Asset)
         The object from which to read data.
-    stacking_library : "odc.stac", "stackstac", optional
-        When stacking multiple items, this argument determines which library
-        to use. Defaults to ``odc.stac`` if available and otherwise ``stackstac``.
+    patch_url : Callable, optional
+        Function that takes a string or pystac object and returns an altered
+        version. Normally used to sign urls before trying to read data from
+        them. For instance when working with Planetary Computer this argument
+        should be set to ``pc.sign``.
+    allow_kerchunk : bool, (True by default)
+        Control whether this reader tries to interpret kerchunk attributes
+        if provided (either in the data-cube extension or as a regular asset
+        with ``references`` or ``index`` as the role).
     """
-    if _is_item_search(obj):
-        item_collection = obj.item_collection()
-        return to_xarray(item_collection, stacking_library=stacking_library, **kwargs)
     raise TypeError
 
 
 @to_xarray.register(pystac.Item)
-@to_xarray.register(pystac.ItemCollection)
 def _(
-    obj: Union[pystac.Item, pystac.ItemCollection],
-    drop_variables: Union[str, List[str], None] = None,
-    stacking_library: Union[Literal["odc.stac", "stackstac"], None] = None,
+    obj: pystac.Item,
+    drop_variables: str | list[str] | None = None,
+    patch_url: None | Callable[[str], str] = None,
+    allow_kerchunk: bool = True,
     **kwargs,
 ) -> xarray.Dataset:
     if drop_variables is not None:
         raise KeyError("``drop_variables`` not implemented for pystac items")
 
-    if stacking_library is None:
-        try:
-            _import_optional_dependency("odc.stac")
-            stacking_library = "odc.stac"
-        except ImportError:
-            _import_optional_dependency("stackstac")
-            stacking_library = "stackstac"
-    elif stacking_library not in ["odc.stac", "stackstac"]:
-        raise ValueError(f"{stacking_library=} is not a valid option")
+    if allow_kerchunk:
+        first_obj = obj if isinstance(obj, pystac.Item) else next(i for i in obj)
+        is_kerchunked = any("kerchunk:" in k for k in first_obj.properties.keys())
+        if is_kerchunked:
+            kerchunk_combine = _import_optional_dependency("kerchunk.combine")
+            fsspec = _import_optional_dependency("fsspec")
 
-    if stacking_library == "odc.stac":
-        odc_stac = _import_optional_dependency("odc.stac")
-        if isinstance(obj, pystac.Item):
-            items = [obj]
-        else:
-            items = [i for i in obj]
-        return odc_stac.load(items, **{"chunks": {"x": 1024, "y": 1024}, **kwargs})
-    elif stacking_library == "stackstac":
-        stackstac = _import_optional_dependency("stackstac")
-        da = stackstac.stack(obj, **kwargs)
-        bands = {}
-        for band in da.band.values:
-            b = da.sel(band=band)
-            scalar_coords = {k: v.item() for k, v in b.coords.items() if v.shape == ()}
-            b = b.assign_attrs(
-                **{k: v for k, v in scalar_coords.items() if v is not None}
-            )
-            b = b.drop_vars(scalar_coords)
-            bands[band] = b
-        return xarray.Dataset(bands, attrs=da.attrs)
+            if isinstance(obj, (list, pystac.ItemCollection)):
+                refs = kerchunk_combine.MultiZarrToZarr(
+                    [_stac_to_kerchunk(item) for item in obj],
+                    concat_dims=kwargs.get("concat_dims", "time"),
+                ).translate()
+            else:
+                refs = _stac_to_kerchunk(obj)
+
+            mapper = fsspec.filesystem("reference", fo=refs).get_mapper()
+            default_kwargs = {
+                "engine": "zarr",
+                "consolidated": False,
+            }
+
+            return xarray.open_dataset(mapper, **{**default_kwargs, **kwargs})
 
 
 def _extract_tar_file(obj):
@@ -92,7 +87,6 @@ def _extract_tar_file(obj):
     pathlib=_import_optional_dependency("pathlib")
     # Extract the archive:href from obj->href ( *.tar ) to a temp dir and pass
     # this  temp dir as the obj.href to xarray
-    #print(f"Encountered tar file:{obj.href}")
     tmppath=os.getenv("STAC_TMP_DIR")
     with tempfile.TemporaryDirectory(dir=tmppath,delete=False) as tmpdirname:
         print(f"tar arch ext file:{obj.extra_fields}")
@@ -103,29 +97,26 @@ def _extract_tar_file(obj):
         if myvar.endswith(('.zarr','.zarr/')):
             #case -1 : tar of zarr .. Complete zarr store
             new_href = os.path.join(tmpdirname,obj.extra_fields["archive:href"])
-            #    myvarFiles = [ tarinfo for tarinfo in mytar.getmembers() if tarinfo.name.startswith(myvar) ]
-            #myvarFiles = [ tarinfo for tarinfo in mytar.getmembers() if tarinfo.name.startswith(myvar)]
         else:
             #case -2 : tar of var or #case -3 : tar of chunk .. Incomplete zarr store
             myvarFiles.extend( [ tarinfo for tarinfo in mytar.getmembers() if tarinfo.name.endswith('.zmetadata') ] )
             myvarFiles.extend( [ tarinfo for tarinfo in mytar.getmembers() if tarinfo.name.endswith('.zgroup') ] )
             zarr_path = obj.extra_fields["archive:href"].split('.zarr')[0] + '.zarr'
             new_href = os.path.join(tmpdirname,zarr_path)
-        #print(myvarFiles)
 
         # TODO: Add a progress bar to help the user 
         mytar.extractall(tmpdirname,myvarFiles)
 
-        #print("Extraction Done!!!")
-        #print(f"tar file new:{new_href}")
     return new_href    
 
 @to_xarray.register
 def _(
-    obj: Union[ pystac.Asset,list],
-    stacking_library: Union[Literal["odc.stac", "stackstac"], None] = None,
+    obj: pystac.Asset,
+    patch_url: None | Callable[[str], str] = None,
+    allow_kerchunk: bool = True,
     **kwargs,
 ) -> xarray.Dataset:
+    open_kwargs = obj.extra_fields.get("xarray:open_kwargs", {})
 
     # MKM 18 Oct 2024
     #TODO : Check if the obj is list instance or pystac.Asset instance and
@@ -140,54 +131,64 @@ def _(
     # Check the type of the 'obj'
     if isinstance(obj, pystac.Asset):
         print("Asset as input!",flush=True)
-        open_kwargs = obj.extra_fields.get("xarray:open_kwargs", {})
+        #open_kwargs = obj.extra_fields.get("xarray:open_kwargs", {})
     
         storage_options = obj.extra_fields.get("xarray:storage_options", None)
         if storage_options:
             open_kwargs["storage_options"] = storage_options
     
-        if obj.media_type == pystac.MediaType.JSON and {"index", "references"}.intersection(
-            set(obj.roles) if obj.roles else set()
-        ):
-            requests = _import_optional_dependency("requests")
-            fsspec = _import_optional_dependency("fsspec")
-            r = requests.get(obj.href)
-            r.raise_for_status()
-            try:
-                import planetary_computer  # type: ignore
+    if (
+        allow_kerchunk
+        and obj.media_type == pystac.MediaType.JSON
+        and {"index", "references"}.intersection(set(obj.roles) if obj.roles else set())
+    ):
+        requests = _import_optional_dependency("requests")
+        r = requests.get(obj.href)
+        r.raise_for_status()
+
+        refs = r.json()
+        if patch_url is not None:
+            refs = patch_url(refs)
+
+        default_kwargs = {
+            "engine": "kerchunk",
+        }
+        return xarray.open_dataset(refs, **{**default_kwargs, **open_kwargs, **kwargs})
+
     
-                refs = planetary_computer.sign(r.json())
-            except ImportError:
-                refs = r.json()
+    if obj.media_type == pystac.MediaType.COG:
+        _import_optional_dependency("rioxarray")
+        default_kwargs = {**default_kwargs, "engine": "rasterio"}
+    elif obj.media_type in ["application/vnd+zarr", "application/vnd.zarr"]:
+        _import_optional_dependency("zarr")
+        zarr_kwargs = {}
+        if "zarr:consolidated" in obj.extra_fields:
+            zarr_kwargs["consolidated"] = obj.extra_fields["zarr:consolidated"]
+        if "zarr:zarr_format" in obj.extra_fields:
+            zarr_kwargs["zarr_format"] = obj.extra_fields["zarr:zarr_format"]
+        default_kwargs = {**zarr_kwargs, "engine": "zarr"}
+    elif obj.media_type == "application/vnd.zarr+icechunk":
+        from xpystac._icechunk import read_icechunk
+
+        return read_icechunk(obj)        
     
-            mapper = fsspec.get_mapper("reference://", fo=refs)
-            default_kwargs = {
-                **default_kwargs,
-                "engine": "zarr",
-                "consolidated": False,
-            }
-            return xarray.open_dataset(
-                mapper, **{**default_kwargs, **open_kwargs, **kwargs}
-            )
+    # MKM added for handling the 'archive' extension, as of now only plain '*.tar'
+    # not zipped tarfiles.
+    elif obj.media_type == "application/x-tar":
+        new_href=_extract_tar_file(obj)
+        print("Extraction Done!!!")
+        print(f"Extracted tar file:{new_href}")
+        # Check the archive:type and set the appropriate engine (as of now 'tar' of 'zarr',
+        # hence 'zarr' engine) to the xarray
+        default_kwargs = {**default_kwargs, "engine": "zarr"}
+        # Pass the new_href and the kwargs to xarray
+        return xarray.open_dataset(new_href, **{**default_kwargs, **open_kwargs, **kwargs})
     
-        if obj.media_type == pystac.MediaType.COG:
-            _import_optional_dependency("rioxarray")
-            default_kwargs = {**default_kwargs, "engine": "rasterio"}
-        elif obj.media_type == "application/vnd+zarr":
-            _import_optional_dependency("zarr")
-            default_kwargs = {**default_kwargs, "engine": "zarr"}
-        # MKM added for handling the 'archive' extension, as of now only plain '*.tar'
-        # not zipped tarfiles.
-        elif obj.media_type == "application/x-tar":
-            new_href=_extract_tar_file(obj)
-            print("Extraction Done!!!")
-            print(f"Extracted tar file:{new_href}")
-            # Check the archive:type and set the appropriate engine (as of now 'tar' of 'zarr',
-            # hence 'zarr' engine) to the xarray
-            default_kwargs = {**default_kwargs, "engine": "zarr"}
-            # Pass the new_href and the kwargs to xarray
-            return xarray.open_dataset(new_href, **{**default_kwargs, **open_kwargs, **kwargs})
-        ds = xarray.open_dataset(obj.href, **{**default_kwargs, **open_kwargs, **kwargs})
+    href = obj.href
+    if patch_url is not None:
+        href = patch_url(href)
+
+        ds = xarray.open_dataset(href, **{**default_kwargs, **open_kwargs, **kwargs})
         return ds
 
     elif isinstance(obj, list):
